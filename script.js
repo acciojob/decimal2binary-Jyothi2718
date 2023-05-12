1a06ceb09ff061022e25c09e70ef8cb7cfd62de4
Jyothirmai <123802567+Jyothi2718@users.noreply.github.com>
2023-05-12 11:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function decimalToBinary(num){
 
-module.exports = threeSum;
+	 if(num===0) return 0;
+      const binary = ' ';
+	
+		 while(num>0){ //7>0; 3>0;1>0
+			binary = (num%2) + binary; // 1+'' =1 +1 = 11+1=111
+			 num = math.floor(num/2); //3; 1
+			 
+		 }
+		return binary; 
+	
+	
+
+	
+}
